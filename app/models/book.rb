0099a5373c2733819540a869aad127433209ee41
Amber <martinez.amber1028@gmail.com>
2022-12-06 02:54:01 -0800

    Adds book validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,8 @@
 class Book < ApplicationRecord
     has_many :reviews
     has_many :users, through: :reviews
+
+    validates :title, :author, :genre, :image_url, presence: true
+    validates :title, uniqueness: true
+    
 end
