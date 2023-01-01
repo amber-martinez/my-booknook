3e265b738ee52e14995ac4365541647aa39d8d70
Amber <martinez.amber1028@gmail.com>
2022-12-31 19:28:39 -0500

    Update build for deployment

--- client/src/Homepage/BookRec.js
+++ client/src/Homepage/BookRec.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
     
 function BookRec() {
     
@@ -14,16 +15,18 @@ function BookRec() {
         .then(r => r.json())
         .then(book => {
             setBookRec(
-                <div style={{ textAlign: 'center', backgroundColor: '#f7f4f1', borderRadius: 10, padding: 30, marginLeft: 0, border: 'solid 1px #f2efeb' }}>
-                    <h3 style={{ fontSize: 18 }}>Book Recommendations</h3>
-                    <p style={{ display: 'inline-block', rightMargin: 50, fontSize: 13, marginBottom: 27 }}>The wise booknook recommends:</p>
-                    <div>
-                        <img src={book.image_url} style={{ height: 135, objectFit: 'cover', overflow: 'hidden', borderRadius: 5 }}></img>
-                        <br></br>
-                        <p style={{ marginTop: 19, fontSize: 12 }}><strong>{book.title}</strong></p>
-                        <p style={{ marginTop: -15, fontSize: 11 }}>{book.author}</p>
+                <Link style={{ color: '#362c24' }} to={`/books/${book.id}`}>
+                    <div style={{ textAlign: 'center', backgroundColor: '#f7f4f1', borderRadius: 10, padding: 30, marginLeft: 0, border: 'solid 1px #f2efeb' }}>
+                        <h3 style={{ fontSize: 18 }}>Book Recommendations</h3>
+                        <p style={{ display: 'inline-block', rightMargin: 50, fontSize: 13, marginBottom: 27 }}>The wise booknook recommends:</p>
+                        <div>
+                            <img src={book.image_url} style={{ height: 135, objectFit: 'cover', overflow: 'hidden', borderRadius: 5 }}></img>
+                            <br></br>
+                            <p style={{ marginTop: 19, fontSize: 12 }}><strong>{book.title}</strong></p>
+                            <p style={{ marginTop: -15, fontSize: 11 }}>{book.author}</p>
+                        </div>
                     </div>
-                </div>
+                </Link>
             )
         })
     }, [])
