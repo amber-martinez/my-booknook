68eb6e018f3869105035637076ac5ba3eebfaa9b
Amber <martinez.amber1028@gmail.com>
2023-01-01 14:46:27 -0500

    Add authors to show books pages

--- client/src/CoreComponents/ShowBooks.js
+++ client/src/CoreComponents/ShowBooks.js
@@ -30,16 +30,16 @@ function ShowBooks() {
                 {bookData.length ? 
                 bookData.map(book => (
                     <Link style={{ color: '#362c24' }} to={`/books/${book.id}`}>
-                        <Card class="card text-center" key={book.id} style={{ display: 'inline-block', border: 'none', backgroundColor: 'transparent', maxWidth: 90, wordBreak: 'break-word', textAlign: 'center', verticalAlign: 'top', maxHeight: 161, margin: 30 }}>
+                        <Card class="card text-center" key={book.id} style={{ display: 'inline-block', border: 'none', backgroundColor: 'transparent', maxWidth: 90, wordBreak: 'break-word', textAlign: 'center', verticalAlign: 'top', maxHeight: 161, margin: '13px 40px 100px 40px' }}>
                             <Card.Img src={book.image_url} style={{ height: 140, width: 90, objectFit: 'cover' }} />
                             <Card.Body style={{ fontSize: 11, textAlign: 'center', marginTop: 10 }}>
                                 {book.average_rating === null ?
-                                <p style={{ marginBottom: 5 }}>No reviews yet</p>
+                                <p style={{ marginBottom: 5 }}>No reviews</p>
                                 :
                                 <Rate disabled allowHalf defaultValue={book.average_rating} style={{ fontSize: 10 }} />
                                 }
-                                <Card.Text style={{ marginTop: 5 }}>
-                                {book.title}</Card.Text>
+                                <Card.Text style={{ marginTop: 5, fontWeight: 600 }}>{book.title}</Card.Text>
+                                <Card.Text style={{ marginTop: -15, fontSize: 10 }}>by {book.author}</Card.Text>
                             </Card.Body>
                         </Card>
                     </Link>
